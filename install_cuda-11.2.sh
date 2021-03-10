42966adc7575aac69c7ea94d558174a22aadbd51
Y.L <yuanl5@alumni.cmu.edu>
2021-03-09 16:33:00 -0800

    Create install_cuda-11.2.sh

---
+++ install_cuda-11.2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage:
+# curl <url to this script> | bash -s <prefix>
+
+CUDA_VERSION=11.2
+TMP_DIR=/tmp/cuda-download-${CUDA_VERSION}
+PREFIX=$1
+
+mkdir -p ${TMP_DIR} && \
+pushd ${TMP_DIR} && \
+wget https://developer.download.nvidia.com/compute/cuda/11.2.1/local_installers/cuda_11.2.1_460.32.03_linux.run && \
+bash cuda_11.2.1_460.32.03_linux.run --silent --toolkit --installpath=${PREFIX}/cuda-${CUDA_VERSION} && \
+popd && \
+rm -rf ${TMP_DIR}

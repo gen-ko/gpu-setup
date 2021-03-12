4b7066e662fffabd3d33bb1186ec2fb40d654277
Y.L <yuanl5@alumni.cmu.edu>
2021-03-11 19:30:31 -0800

    Create install_conda.sh

---
+++ install_conda.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Usage:
+# curl https://raw.githubusercontent.com/gen-ko/gpu-setup/main/install_conda.sh | bash -s <prefix>
+
+
+TMP_DIR=/tmp/conda-download
+PREFIX=$1
+
+mkdir -p ${TMP_DIR} && \
+pushd ${TMP_DIR} && \
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh && \
+bash Anaconda-latest-Linux-x86_64.sh -p ${PREFIX} && \
+popd && \
+rm -rf ${TMP_DIR}

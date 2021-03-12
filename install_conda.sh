6538064dccecd0cea4786fd2e7b6a51dffd48dc8
Y.L <yuanl5@alumni.cmu.edu>
2021-03-11 19:32:25 -0800

    Update install_conda.sh

--- install_conda.sh
+++ install_conda.sh
@@ -2,6 +2,7 @@
 
 # Usage:
 # curl https://raw.githubusercontent.com/gen-ko/gpu-setup/main/install_conda.sh | bash -s <prefix>
+# <prefix> should be a non-exist directory like ~/anaconda
 
 
 TMP_DIR=/tmp/conda-download

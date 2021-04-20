c613ccaeaaf5592842b65f6f552c160eebc467ba
Y.L <yuanl5@alumni.cmu.edu>
2021-04-19 18:57:53 -0700

    Update install_cuda.sh

--- install_cuda.sh
+++ install_cuda.sh
@@ -4,6 +4,7 @@
 # curl https://raw.githubusercontent.com/gen-ko/gpu-setup/main/install_cuda.sh | bash -s <prefix> <cuda-version>
 # works with CentOS7/8 | Debian 10 | Fedora 33 | OpenSUSE 15 | RHEL 7/8 | SLES 15 | Ubuntu 20.04/18.04/16.04 | WSL-Ubuntu 2.0
 CUDA_VERSION=${2:-11.2}
+echo "CUDA_VERSION:${CUDA_VERSION}"
 TMP_DIR=/tmp/cuda-download-${CUDA_VERSION}
 PREFIX=$1
 
